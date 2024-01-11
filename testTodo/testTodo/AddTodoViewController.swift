2d8c3ca35efed95a008be5c2cb5a5cd5e6f886fb
GABIN KIM <144803017+BINDUNGLIFE@users.noreply.github.com>
2024-01-11 12:28:59 +0900

    Add files via upload

--- testTodo/testTodo/AddTodoViewController.swift
+++ testTodo/testTodo/AddTodoViewController.swift
@@ -8,73 +8,62 @@
 import UIKit
 
 class AddTodoViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var contentTextView: UITextView!
+    var didAddHandler: ((Todo) -> Void)?
+    
+    
+    @IBAction func addListItemAction(_ sender: Any) {
+        guard let title = titleTextField.text, !title.isEmpty else {
+                // 사용자에게 제목을 입력하라는 경고를 표시할 수 있습니다.
+                // 예: 알림 대화상자를 표시하는 코드를 여기에 추가합니다.
+                return
+        }
+        
+        let content = contentTextView.text ?? ""
+            let newTodo = Todo(title: title, content: content, isComplete: false, category: "Work")
+            
+            // TodoListViewController로 새 Todo 객체를 전달하기 위해 핸들러를 호출합니다.
+            didAddHandler?(newTodo)
+            
+            // 현재 뷰 컨트롤러를 닫고 이전 화면으로 돌아갑니다.
+            self.navigationController?.popViewController(animated: true)
+        }
+    
+    
+    @IBAction func cancelButtonAction(_ sender: Any) {
+        self.navigationController?.popViewController(animated: true)
     }
     
-    //
-    //  ViewController.swift
-    //  testTodo
-    //
-    //  Created by 김가빈 on 12/20/23.
-    //
-
-
-    class AddTodoViewController: UIViewController {
-
-        @IBOutlet weak var titleTextField: UITextField!
-        @IBOutlet weak var contentTextView: UITextView!
-        var didAddHandler: ((TodoList) -> Void)?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
         
-        @IBAction func addListItemAction(_ sender: Any) {
-            let title = titleTextField.text!
-            let content = contentTextView.text ?? ""
-            
-            let item: TodoList = TodoList(title: title, content: content)
-            
-            print("Add List title : \(item.title)")
-            // TodoListViewController에 생성한 전역변수에 append
-            list.append(item)
-            
-            // Done 버튼 클릭 시 새로운 TodoList 객체 생성
-            // 생성한 객체에 입력한 정보 저장
-            self.navigationController?.popViewController(animated: true)
-        }
-
-        @IBAction func cancelButtonAction(_ sender: Any) {
-            self.navigationController?.popViewController(animated: true)
-        }
-        
-        override func viewDidLoad() {
-            super.viewDidLoad()
-
-            // Do any additional setup after loading the view.
-            contentTextView.layer.borderColor = UIColor.gray.cgColor
-            contentTextView.layer.borderWidth = 0.3
-            contentTextView.layer.cornerRadius = 2.0
-            contentTextView.clipsToBounds = true
-        }
-
-        override func didReceiveMemoryWarning() {
-            super.didReceiveMemoryWarning()
-            // Dispose of any resources that can be recreated.
-        }
+        // Do any additional setup after loading the view.
+        titleTextField.placeholder = "제목을 입력하세요"
+        contentTextView.layer.borderColor = UIColor.gray.cgColor
+        contentTextView.layer.borderWidth = 0.3
+        contentTextView.layer.cornerRadius = 2.0
+        contentTextView.clipsToBounds = true
     }
-
-
-
-    /*
-     
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
-    */
-
 }
+
+
+
+/*
+ 
+ // MARK: - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+ // Get the new view controller using segue.destination.
+ // Pass the selected object to the new view controller.
+ }
+ */
+
--- testTodo/testTodo/TodoListViewController.swift
+++ testTodo/testTodo/TodoListViewController.swift
@@ -1,85 +1,210 @@
 import UIKit
 
-// 할일 저장 리스트 (전역변수)
-var list = [TodoList]()
+
+// 할 일 항목의 데이터 모델을 정의
+struct Todo {
+    var title: String
+    var content: String
+    var isComplete: Bool
+    var category: String
+    
+    // 새로운 항목을 생성하고 초기화
+    init(title: String, content: String, isComplete: Bool, category: String) {
+        self.title = title
+        self.content = content
+        self.isComplete = isComplete
+        self.category = category
+        
+    }
+}
 
 class TodoListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
-
-    /// 할일리스트를 보여주는 테이블 뷰
-
+    // 항목을 저장하는데 사용
+    var list = [Todo]()
+    var didAddHandler: ((Todo) -> Void)?
+    // 섹션별로 나눈 Todo배열
+    var categorizedTodos: [String: [Todo]] = [:]
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showAddTodoView" { // fSegue의 Identifier를 확인합니다.
+            if let addTodoVC = segue.destination as? AddTodoViewController {
+                // 핸들러를 정의하여 TodoListViewController의 테이블뷰를 갱신합니다.
+                addTodoVC.didAddHandler = { [weak self] newTodo in
+                    // 새로운 Todo를 list 배열에 추가합니다.
+                    self?.list.append(newTodo)
+                    // 카테고리별로 Todo를 다시 분류하고, 테이블 뷰를 갱신합니다.
+                    self?.categorizeTodos()
+                    self?.TodoListTableView.reloadData()
+                }
+            }
+        }
+    }
+    
+    //데이터를 카테고리별로 나누는 로직
+    func categorizeTodos() {
+        categorizedTodos = [:] //초기화
+        for todo in list {
+            categorizedTodos[todo.category, default: []].append(todo)
+        }
+    }
+    // 섹션의 개수를 반환
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return categorizedTodos.keys.count
+    }
+    
+    
+    // 헤더를 반환
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return Array(categorizedTodos.keys)[section]
+    }
+    
+    // 푸터를 반환
+    func tableView(_ tableView: UITableView, titleForFooterInSection section: Int) -> String? {
+        return "This is \(Array(categorizedTodos.keys)[section]) Footer"
+    }
+    
+    // 각 섹션에 해당하는 로우의 개수를 반환
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let categoryKey = Array(categorizedTodos.keys)[section]
+        return categorizedTodos[categoryKey]?.count ?? 0
+    }
+    
+    // 셀을 구성
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let categoryKey = Array(categorizedTodos.keys)[indexPath.section]
+        if let todos = categorizedTodos[categoryKey], indexPath.row < todos.count {
+            let todo = todos[indexPath.row]
+            cell.textLabel?.text = todo.title
+            cell.detailTextLabel?.text = todo.content
+            cell.accessoryType = todo.isComplete ? .checkmark : .none
+            // 체크마크 설정
+            cell.accessoryType = todo.isComplete ? .checkmark : .none
+        }
+        return cell
+    }
+    
     @IBOutlet weak var TodoListTableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        TodoListTableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        
         // 네비게이션 아이템에 편집 버튼 추가
         self.navigationItem.leftBarButtonItem = editButtonItem
         
         // 테이블 뷰의 delegate와 dataSource 설정
         TodoListTableView.delegate = self
         TodoListTableView.dataSource = self
-
+        
         // 데이터 로드
         loadAllData()
     }
-
+    
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        saveAllData()
         TodoListTableView.reloadData()
     }
-
-    // MARK: - Data Management
+    
+    override func setEditing(_ editing: Bool, animated: Bool) {
+        super.setEditing(editing, animated: animated)
+        TodoListTableView.setEditing(editing, animated: animated)
+    }
+    
+    
+    
     func saveAllData() {
         let data = list.map {
             [
                 "title": $0.title,
-                "content": $0.content ?? "",
+                "content": $0.content ,
                 "isComplete": $0.isComplete
             ]
         }
-        let userDefaults = UserDefaults.standard
-        userDefaults.set(data, forKey: "items")
-        userDefaults.synchronize()
+        UserDefaults.standard.set(data, forKey: "items")
     }
-
+    
     func loadAllData() {
-        let userDefaults = UserDefaults.standard
-        guard let data = userDefaults.object(forKey: "items") as? [[String: AnyObject]] else { return }
-        list = data.map {
-            let title = $0["title"] as? String ?? ""
-            let content = $0["content"] as? String
-            let isComplete = $0["isComplete"] as? Bool ?? false
-            return TodoList(title: title, content: content, isComplete: isComplete)
+        if let data = UserDefaults.standard.object(forKey: "items") as? [[String: Any]] {
+            list = data.map { dict in
+                guard let title = dict["title"] as? String,
+                      let content = dict["content"] as? String,
+                      let isComplete = dict["isComplete"] as? Bool,
+                      let category = dict["category"] as? String else {
+                    return Todo(title: "", content: "", isComplete: false, category: "")
+                }
+                return Todo(title: title, content: content, isComplete: isComplete, category: category)
+            }
+            categorizeTodos()
         }
     }
-
-    // MARK: - UITableView DataSource & Delegate Methods
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return list.count
-    }
-
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = list[indexPath.row].title
-        cell.detailTextLabel?.text = list[indexPath.row].content
-        cell.accessoryType = list[indexPath.row].isComplete ? .checkmark : .none
-        return cell
-    }
-
+    
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            list.remove(at: indexPath.row)
+            let categoryKey = Array(categorizedTodos.keys)[indexPath.section]
+            
+            // 먼저 list 배열에서 해당 항목을 찾아 제거합니다.
+            if let todoToRemove = categorizedTodos[categoryKey]?[indexPath.row] {
+                list.removeAll { $0.category == categoryKey && $0.title == todoToRemove.title }
+            }
+            
+            // 그 다음 categorizedTodos에서 해당 항목을 제거합니다.
+            categorizedTodos[categoryKey]?.remove(at: indexPath.row)
+            
+            // 테이블 뷰에서 해당 셀을 삭제
             tableView.deleteRows(at: [indexPath], with: .fade)
+            
+            // 데이터 저장
+            saveAllData()
         }
     }
-
+    
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        guard !list[indexPath.row].isComplete else { return }
-        list[indexPath.row].isComplete = true
-        let dialog = UIAlertController(title: "Todo List", message: "일을 완료했습니다!!!!!", preferredStyle: .alert)
-        let action = UIAlertAction(title: "확인", style: .default)
-        dialog.addAction(action)
-        self.present(dialog, animated: true, completion: nil)
-        tableView.reloadData()
+        // 카테고리와 일치하는 Todo 항목을 찾습니다.
+        let categoryKey = Array(categorizedTodos.keys)[indexPath.section]
+        if var todos = categorizedTodos[categoryKey] {
+            // 선택된 Todo 항목의 완료 상태를 변경합니다.
+            todos[indexPath.row].isComplete = true // 또는 .toggle()로 토글할 수 있습니다.
+            // 변경된 완료 상태를 전역 Todo 배열에도 반영합니다.
+            if let indexInList = list.firstIndex(where: { $0.title == todos[indexPath.row].title && $0.category == categoryKey }) {
+                list[indexInList].isComplete = todos[indexPath.row].isComplete
+            }
+            // 변경된 데이터로 categorizedTodos를 업데이트합니다.
+            categorizedTodos[categoryKey] = todos
+            // 테이블뷰의 해당 셀을 갱신합니다.
+            tableView.reloadRows(at: [indexPath], with: .none)
+            
+            // 할 일 완료 알림을 표시합니다.
+            let dialog = UIAlertController(title: "Todo List", message: "할 일을 완료했습니다.", preferredStyle: .alert)
+            let action = UIAlertAction(title: "확인", style: .default) { _ in
+                // 알림을 닫은 후에 할 작업이 있다면 여기에 작성합니다.
+            }
+            dialog.addAction(action)
+            self.present(dialog, animated: true, completion: nil)
+        }
+        
+        // 선택된 셀의 선택 상태를 해제합니다.
+        tableView.deselectRow(at: indexPath, animated: true)
     }
+
+    
+    func updateTodoItem(at index: Int, with title: String, content: String) {
+        if list.indices.contains(index) {
+            // 배열 업데이트
+            list[index].title = title
+            list[index].content = content
+            
+            // UserDefaults 업데이트
+            saveAllData()
+            
+            // 테이블 뷰 업데이트
+            TodoListTableView.reloadRows(at: [IndexPath(row: index, section: 0)], with: .automatic)
+            
+        }
+        
+    }
+    
 }
+
+
